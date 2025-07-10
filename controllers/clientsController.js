78b343eb7012f97686a8ac3b0c98ffe5308bb431
danielsangronis1386 <daniel.sangronis1386@gmail.com>
2025-07-09 23:37:55 -0700

    Show a single client and their notes

--- controllers/clientsController.js
+++ controllers/clientsController.js
@@ -23,6 +23,25 @@ const create = async (req, res) => {
         const newClient = new Client({
             ...req.body,
             owner: req.session.user._id,
-        })
+        });
+        await newClient.save();
+        res.redirect("/clients");
+    } catch (error) {
+      console.error(error);
+      res.redirect("/clients/new");
+    }
+};
+
+//Show a single client and their notes 
+
+const show = async (req, res) => {
+    try {
+        const client = await Client.findOne ({ _id: req.params.id, owner: req.session.user._id})
+        .populate("notes");
+        res.render("clients/show", {client});
+    } catch (error) {
+        console.error(error);
+        res.redirect("/clients")
     }
 }
+
